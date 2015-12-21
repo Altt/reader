8b3c5a4e5b8cc9c28662d55694d36f82d0d83c23
Altt <altt.rf@gmail.com>
2015-12-21 19:14:45 +0300

    1243

---
+++ src/Model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by altt on 21.12.15.
+//
+
+#include "Model.h"
---
+++ src/Model.h
@@ -0,0 +1,8 @@
+//
+// Created by altt on 21.12.15.
+//
+
+#ifndef READER_MODEL_H
+#define READER_MODEL_H
+
+#endif //READER_MODEL_H
